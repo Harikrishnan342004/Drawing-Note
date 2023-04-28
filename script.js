472751b49880710fea6e8b3035fa96b7d17f0c75
Hari <harikrishnan53828@gmail.com>
2023-04-28 13:00:44 +0530

    heheh

---
+++ script.js
@@ -0,0 +1,58 @@
+let canvas = document.getElementById("canvas");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+let ctx = canvas.getContext("2d");
+ctx.lineWidth = 4;
+
+
+
+
+let prevX = null;
+let prevY = null;
+
+let draw = false;
+
+
+window.addEventListener("mousedown" , (e) => draw = true);
+window.addEventListener("mouseup" , (e) =>   draw = false);
+
+window.addEventListener("mousemove" , function(e)
+{
+    if(prevX == null || prevY == null || !draw)
+    {
+        prevX = e.clientX;
+        prevY = e.clientY;
+        return;
+    }
+
+let mouseX = e.clientX;
+let mouseY = e.clientY;
+ctx.beginPath();
+ctx.moveTo(prevX , prevY);
+ctx.lineTo(mouseX, mouseY);
+ctx.stroke();
+
+prevX = e.clientX;
+prevY = e.clientY;
+ 
+})
+
+
+
+
+
+let clrs = document.querySelectorAll(".clr");
+clrs = Array.from(clrs);
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr;
+    })
+})
+
+
+//Clearing Canvas
+
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
